5c6755b2f14c2ebb7f2d20a5f36d4a3be28d5de6
duncanmichel <25329325+duncanmichel@users.noreply.github.com>
2019-01-10 11:58:52 -0500

    Update misc.sh

--- HackerRank/Bash/misc.sh
+++ HackerRank/Bash/misc.sh
@@ -10,7 +10,6 @@ for i in {1..99..2}
  done
 
 
-
 BEGINCOMMENT
   Four lines containing the sum (), difference (), product (), and quotient (), respectively. 
 ENDCOMMENT
@@ -21,3 +20,22 @@ echo `expr $num1 + $num2`
 echo `expr $num1 - $num2`
 echo `expr $num1 \* $num2`
 echo `expr $num1 / $num2`
+
+
+BEGINCOMMENT
+  Given two integers, X and Y, identify whether X>Y or X<Y or X=Y.
+Exactly one of the following lines: 
+- X is less than Y 
+- X is greater than Y 
+- X is equal to Y
+ENDCOMMENT
+
+read num1
+read num2
+if [ $num1 -gt $num2 ]; then #VERY sensitive to spacing in the conditional
+    echo "X is greater than Y"
+elif [ $num1 -lt $num2 ]; then
+    echo "X is less than Y"
+elif [ $num1 -eq $num2 ]; then
+    echo "X is equal to Y"
+fi
